7c76534970d551ee69277d6a357822a2b7edb1c4
TCMasinga <masingacharmaine@gmail.com>
2023-07-26 19:06:43 -0700

    functions and specifiers

--- _printf.c
+++ _printf.c
@@ -1,25 +1,6 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-#include "format_helpers.h"
-
-#define FORMAT_HELPERS_H
-#define BUFF_SIZE 1024
 
 void print_buffer(char buffer[], int *buff_ind);
-/**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-	{
-		write(1, buffer, *buff_ind);
-		*buff_ind = 0;
-	}
-}
 
 /**
  * _printf - produces output according to a format.
@@ -73,5 +54,19 @@ int _printf(const char *format, ...)
 
 	va_end(list);
 
-	return printed_chars;
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+	{
+		write(1, &buffer[0], *buff_ind);
+		*buff_ind = 0;
+	}
 }
